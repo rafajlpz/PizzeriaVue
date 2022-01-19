0f27ccc318d9250b59802fb24110a04721d1b318
webferrol <xurxo@webferrol.com>
2022-01-19 12:48:58 +0100

    Ejercicio vue que simula una pizzería
    -Se usa cdn
    -Instrucciones en src/main.js

---
+++ src/components/MainFooter.js
@@ -0,0 +1,8 @@
+export default {
+    /* plugin 'es6 string html' para ver en colorines */
+    template: /*html*/`
+        <footer class="main-footer">
+        Componente de @webferrol
+        </footer>
+        `,
+}
---
+++ src/main.js
@@ -0,0 +1,19 @@
+/**
+ * CONCEPTOS
+ * -Interpolación {{}}
+ * -Directivas v-bind, v-on, v-for, v-if, v-if-else, v-else, v-key
+ */
+
+/**
+ * 1. Crear el componente principal y montarlo sobre el nodo #app
+ * 2. Crear el modelo que tendrá como propiedades: 
+ * sitio (string)
+ * pizzas (array) Lista de pizzas 
+ * pizzaSeleccionada (obj) Pizza seleccionada
+ * carrito (array) Donde almacenamos los productos seleccionados
+ * 
+ * 3. Listar de forma correcta los productos leyendo el archivo pizzas.json. Esto será al cargar la página
+ * 4. Al seleccionar una pizza cambiará la imagen y el título de la pizza seleccionda
+ * 4. Añadir y quitar el precio total de las pizzas selecciondas
+ * 
+ */
